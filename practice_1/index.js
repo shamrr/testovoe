aa14b3045dfa61333398ecbfcc6ee57203455be8
Nastya <shamr44@mail.ru>
2023-09-21 12:06:47 +0300

    feat: wrote functions for add and remove image

--- practice_1/index.js
+++ practice_1/index.js
@@ -1,4 +1,8 @@
 const overlay = document.querySelector('.overlay');
+const fileInput = document.querySelector('#logo');
+const logoImg = document.querySelector('.logo__img');
+const select = document.querySelector('.select');
+const logoLabel = document.querySelector('.logo__label');
 
 const openModal = () => {
     overlay.classList.add('active')
@@ -12,4 +16,25 @@ const closeModalByClickedOverlay = (event) => {
     if(event.target === overlay) {
         closeModal();
     }
+}
+
+const showImage = () => {
+    if (fileInput.files[0]) {
+        const reader = new FileReader();
+        
+        reader.onload = function (e) {
+            logoImg.src = e.target.result;
+        };
+        
+        select.style.display = 'none';
+        logoLabel.style.display = 'none';
+
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+const deleteImage = () => {
+    logoImg.src = 'img/logo.png';
+    select.style.display = 'block';
+    logoLabel.style.display = 'block';
 }
