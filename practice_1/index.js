c9dd84e64f79a4dedf518c90c735bd34422a8a43
Nastya <shamr44@mail.ru>
2023-09-26 00:52:29 +0300

    feat: add function for input with phone

--- practice_1/index.js
+++ practice_1/index.js
@@ -58,3 +58,27 @@ const handleSubmit = (event) =>{
 const resetForm = () => {
     form.reset();
 }
+
+//маска для пароля
+const createMaskForPhone = (event) => {
+    const input = event.target;
+    const value = input.value.replace(/\D/g, ''); // Убираем все нечисловые символы
+
+    if (value.length <= 1) {
+        input.value = '+';
+    } 
+    if (value.length <= 4) {
+        input.value = `+${value}`;
+    }
+    if (value.length <= 7) {
+        input.value = `+${value.slice(0, 1)} ${value.slice(1, 4)}`;
+    }
+    if (value.length <= 11) {
+        input.value = `+${value.slice(0, 1)} ${value.slice(1, 4)} ${value.slice(4, 7)}-${value.slice(7, 9)}`;
+    } 
+    if (value.length <= 15){
+        input.value = `+${value.slice(0, 1)} ${value.slice(1, 4)} ${value.slice(4, 7)}-${value.slice(7, 9)}-${value.slice(9, 11)}`;
+    }
+};
+
+
